3639bd418a1c60794e0b948ac6d73eee7b943de6
Diego A. Cacilha <dacacilha@cbsistemas.com.br>
2018-05-15 13:32:43 -0300

    Atividade feito no trampo

--- src/prova/Memoria.java
+++ src/prova/Memoria.java
@@ -5,10 +5,21 @@ public class Memoria {
     
     public Memoria(int tamanho){
         this.unidadeAlocacao = new Unidade(tamanho);
+        this.unidadeAlocacao.setProximo(unidadeAlocacao);//inicialmente a lista será vazia, então o próximo e anterior apontam pra ela mesma
+        this.unidadeAlocacao.setAnterior(unidadeAlocacao);
     }
-    
+    /**
+     * A lógica básica para fazer a lista encadeada é mais ou menos essa. 
+     * Só temos que subtrair o tamanho da nova unidade da unidadeAlocacao
+     * @param tamanho Tamanho da unidade que será alocada
+     */
     public void alocar(int tamanho){
-        
+        Unidade temp = new Unidade(tamanho);
+        temp.setLivre(false);
+        if(this.unidadeAlocacao.temAnterior()) this.unidadeAlocacao.setAnterior(temp);
+        temp.setProximo(this.unidadeAlocacao.getProximo());
+        temp.setAnterior(this.unidadeAlocacao);
+        this.unidadeAlocacao.setProximo(temp);
     }
     
 }
--- src/prova/Unidade.java
+++ src/prova/Unidade.java
@@ -3,13 +3,34 @@ package prova;
 public class Unidade {
     private boolean livre = true;
     private int tamanho;
+    
+    private Unidade anterior = null;
     private Unidade proximo = null;
     
     public Unidade(int tamanho){
         this.tamanho = tamanho;
     }
     
-    public void subtrairTamanho(int tamanho){
-        this.tamanho = this.tamanho - tamanho;
+    public boolean getLivre(){
+        return this.livre;
+    }
+    
+    public boolean temAnterior(){
+        return !this.anterior.equals(null);
+    }
+    public Unidade getProximo(){
+        return this.proximo;
+    }
+    public Unidade getAnterior(){
+        return this.anterior;
+    }
+    public void setLivre(boolean b){
+        this.livre = b;
+    }
+    public void setProximo(Unidade uni){
+        this.proximo = uni;
+    }
+    public void setAnterior(Unidade uni){
+        this.anterior = uni;
     }
 }
